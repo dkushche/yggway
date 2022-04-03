3cc69b211e6360283154776765a439c28efa8f1b
Dmytro Kushchevskyi <d.kushchevskyi@sirinsoftware.com>
2022-04-03 14:16:15 +0300

    Fix interaction with ygg and alf

--- scripts/start_gateway.sh
+++ scripts/start_gateway.sh
@@ -5,11 +5,13 @@ docker build --tag alfis_resolver_image alfis
 
 ./scripts/stop_gateway.sh
 
+mkdir -p storage
+
 docker run --name yggdrasil_gateway_container \
            --network="host" \
            --cap-add=NET_ADMIN \
            --device=/dev/net/tun:/dev/net/tun \
-           --volume $PWD/yggdrasil:/yggdrasil \
+           --volume $PWD/storage:/mnt/storage \
            --detach --rm --interactive --tty yggdrasil_gateway_image
 
 resolv_conf=`cat /etc/resolv.conf`
@@ -18,5 +20,5 @@ echo -e "# Alfis DNS resolver\nnameserver 127.0.0.1\n$resolv_conf" \
 
 docker run --name alfis_resolver_container \
            --network="host" \
-           --volume $PWD/alfis:/alfis \
+           --volume $PWD/storage:/mnt/storage \
            --detach --rm --interactive --tty alfis_resolver_image
